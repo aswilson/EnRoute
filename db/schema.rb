2dc36d53a9707143242d10168256fe87801feba2
Allie Wilson <aswilson@andrew.cmu.edu>
2015-04-01 14:10:40 -0400

    fixed issues in database.yml so that the database setup is now correct and doesn't hinder anything else

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150304041559) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "business_services", force: true do |t|
     t.integer  "business_id"
     t.integer  "service_id"
