5dd01c2f3ec1d19708c8ccfba542f2d6a533bd77
Allie Wilson <aswilson@andrew.cmu.edu>
2015-04-02 15:16:59 -0400

    added zip_code to user as didn't include in prior migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402190940) do
+ActiveRecord::Schema.define(version: 20150402191630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20150402190940) do
     t.string   "street_2"
     t.string   "city"
     t.string   "state"
+    t.string   "zip_code"
   end
 
 end
