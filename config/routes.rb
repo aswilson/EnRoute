93ddd6293fce5f3899829d479c8d0993b4e73e0d
Allie Wilson <aswilson@andrew.cmu.edu>
2015-04-28 14:55:45 -0400

    set up nav and footer partials if needed

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ EnRoute::Application.routes.draw do
   
   get 'welcome/getAllNearby', to: 'welcome#getAllNearby'
   
-  get 'home', to: 'welcome#index', as: :home
+  get 'home', to: 'welcome#base_map', as: :home
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
