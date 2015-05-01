bceaf9cb409e8ebca223819f3c2a8276ff75c7e1
Allie Wilson <aswilson@andrew.cmu.edu>
2015-05-01 15:16:58 -0400

    added routes for about, feedback, privacy

--- config/routes.rb
+++ config/routes.rb
@@ -30,9 +30,9 @@ EnRoute::Application.routes.draw do
   
   get 'welcome/getAllNearby', to: 'welcome#getAllNearby'
   
-  get 'welcome/about', to: 'welcome#about'
-  get 'welcome/privacy', to: 'welcome#privacy'
-  get 'welcome/feedback', to: 'welcome#feedback'
+  get 'about' => 'welcome#about', as: :about
+  get 'privacy' => 'welcome#privacy', as: :privacy
+  get 'feedback' => 'welcome#feedback', as: :feedback
   
   get 'home', to: 'welcome#base_map', as: :home
   # The priority is based upon order of creation: first created -> highest priority.
